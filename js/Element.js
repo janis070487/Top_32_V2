43fb82e484e4b58d9b52f331432e7fd0bedc6bd3
janis070487 <janis070487@gmail.com>
2024-10-19 01:39:37 +0300

    Izdomāju pirmo klassi kas darbojas

--- js/Element.js
+++ js/Element.js
@@ -1,96 +1,140 @@
 
+
+
+
+//_________________________________________________________________________________________
+
+
 /**
- * dddd
+ * Izveido Bootstram pogu
+ * @param {string} ID pievieno objekta idetifikators
+ * @param {string} color pievieno pogas krasu 
+ * iespejamie varianti:
+ * Primary, Secondary, Success, Danger, Warning, Info, Light, Dark
+ * @param {string} TextNode Iestata pogas uzrakstu - Pec noklusejuma: " "
  */
-
-//_________________________________________________________________________________________
-
-class _Element {
-  constructor(ID, parentName, type){
+class B_Outline_buttons {
+  constructor(ID, color, TextNode=" "){
+    
     this.ID = ID;
-    this.parentName = parentName;
-    this.type = type;
-    this._atribute = new Array(0);
-    this._atributeValue = new Array(0);
-    this._class = new Array(0);
+    this.parentName;
+    this.TextNode = TextNode;
+    this._element = document.createElement("button");
+    this._element.setAttribute("type", "button");
+    this._element.setAttribute("id", ID);
+    let textNode = document.createTextNode(TextNode);
+    this._element.appendChild(textNode);
+    let _classList = this._element.classList;
+    _classList.add("btn");
+     switch (color) {
+       case "Primary":
+         _classList.add("btn-outline-primary");
+       break;
+      case "Secondary":
+        _classList.add("btn-outline-secondary");
+        break;
+      case "Success":
+        _classList.add("btn-outline-success");
+        break;
+      case "Danger":
+        _classList.add("btn-outline-danger");
+        break;
+      case "Warning":
+        _classList.add("btn-outline-warning");
+        break;
+      case "Info":
+        _classList.add("btn-outline-info");
+        break;
+      case "Light":
+        _classList.add("btn-outline-light");
+        break;
+      case "Dark":
+        _classList.add("btn-outline-dark");
+        break;
+        default:
+          _classList.add("btn-outline-secondary");
+          break;
+     }
+   
   }
-  show(){
 
+  
+  /**
+   * 
+   * @param {string} parentName elementa identifikators kur elements tiks ievietots
+   */
+  show(parentName = " "){
+      
+      document.getElementById(parentName).appendChild(this._element);
   }
-  addClass(how){
-    this._class.push(how);
-  }
+
+  
 }
 
-//_________________________________________________________________________________________
-
-class B_Element_BTN extends _Element {
-  constructor(ID, parentName){
-    this.ID = ID;
-    this.parentName =parentName;
-  }
-}
 
 //_________________________________________________________________________________________
 
 
 /*
-q   <nav class="navbar navbar-dark bg-dark fixed-top">
-w   <div class="container-fluid">
-e     <a class="navbar-brand" href="#">Top 32</a>
-r     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-t       <span class="navbar-toggler-icon"></span>
-  </button>
-y     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-u       <div class="offcanvas-header">
-!      <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>   virsraksts
-i         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-o       <div class="offcanvas-body">
-p        <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
-     
 
+*/
+//_____________________________________ Turpinam so ___________________________________
+/*
+
+<nav class="navbar navbar-dark bg-dark fixed-top">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Top 32</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Top 32 Izvēle</h5>
+      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      
+      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
         <li class="nav-item">
-          <button type="button" class="btn btn-outline-secondary">Home</button>
+          <button type="button" class="btn btn-outline-secondary">Secondary</button>
         </li>
 
-    
-      </ul> 
+        <li class="nav-item">
+          <button type="button" class="btn btn-outline-secondary">Secondary</button>
+        </li>
+
+        <li class="nav-item">
+        <button type="button" class="btn btn-outline-secondary">Secondary</button>
+        </li>
+
+        <li class="nav-item">
+        <button type="button" class="btn btn-outline-secondary">Secondary</button>
+        </li>
+       
+      </ul>
+      
     </div>
   </div>
-
 </div>
-
 </nav>
+
 */
 
-class B_Offcanvas_Dark_Navbar extends _Element {
+class B_Offcanvas_Dark_Navbar {
 
-  constructor(ID, parentName){
-    this.ID = ID;
-    this.parentName =parentName;
-    this._element = new Array(0);
-    this._id_breakpoint = new Array(0);  // 
-
-    let q = new _Element(this.ID + "q", ID, "nav")
-    q.addClass("navbar");
-    q.addClass("navbar-dark");
-    q.addClass("fixed-top");
-    this._element.push(q);
-    
-    let w = new _Element(this.ID + "w", this.ID + "q", "div");
-    w.addClass("container-fluid");
-    this._element.push(w);
+  
     
 
 
-  }
+  
 
   addMenu(){
 
   }
 }
+  //________________________________________________________________________
+  
 
 //_________________________________________________________________________________________
 
--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ for (let x in _Element) {
 
 // Display the Text
 */
+
+
+
+// let el1 = new _Element("BT1", "start", "button");
+// el1.addAtribute("type", "button");
+// el1.addClass("btn");
+// el1.addClass("btn-outline-secondary");
+// el1._node = "Janis";
+// el1.show();
+
+//  <button type="button" class="btn btn-outline-secondary">Secondary</button>
+
+//let bt1 = B_Outline_buttons("Test", "Secondary", "Janis");
+let bt1 = new B_Outline_buttons("btn1", "Secondary", "Home");
+//bt1.show("start");
+ 
+//document.getElementById("start").appendChild(para);
